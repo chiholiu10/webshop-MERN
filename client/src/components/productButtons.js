db097f7847b40685e6473f34ae8151c2aaa92811
Chi Ho <>
2020-05-27 00:46:52 +0200

    productButtons

--- client/src/components/productButtons.js
+++ client/src/components/productButtons.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteItem, editItem, addItem } from '../actions/index';
 
-export const ProductButtons = ({ productId, index, productEditable}) => {
+export const ProductButtons = ({ productId, index, productEditable, title, description, amount }) => {
     const dispatch = useDispatch();
     return (
         <div>
             <button onClick={() => dispatch(deleteItem(productId, index))}>Delete</button>
-            {productEditable ?   <button type="submit" >Save</button> :   <button onClick={() => dispatch((editItem(productId, index)))}>Edit</button>}
-            <button onClick={() => dispatch(addItem(productId))}>Add</button>
+            {productEditable ? <button type="submit" >Save</button> : <button onClick={() => dispatch((editItem(productId, index)))}>Edit</button>}
+            <button onClick={() => dispatch(addItem(title, description, amount))}>Add</button>
         </div>
     )
 }
