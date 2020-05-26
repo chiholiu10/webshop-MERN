58edce07177fbfbf6405f0563d20db84d5e7abb0
Chi Ho <>
2020-05-27 00:41:25 +0200

    Cart added in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Input from './components/input';
 import ProductList from './components/productList';
 import Searchbar from './components/searchBar';
+import Cart from './components/cart';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="navbar">
+          <Cart/>
+        </div>
         <Searchbar/>
         <Input/>
         <ProductList/>
