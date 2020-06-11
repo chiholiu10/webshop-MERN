6e9907b7e18db70e678aa08c745d3c0cbd8f29b2
Chi Ho <>
2020-06-12 01:50:18 +0200

    move element

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="navbar">
-          <Cart/>
-        </div>
+        <Cart/>
         <Searchbar/>
         <Input/>
         <ProductList/>
